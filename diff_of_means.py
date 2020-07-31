0cd6cc92b50cbdd20d4b09908e88ca7180741e4a
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-07-31 18:03:14 +0200

    Arrays Mean Difference

---
+++ diff_of_means.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
